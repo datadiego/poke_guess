a2b725457ef17d45cf5026b1602f36ebde782f1d
Juan Diego <juandiegomariscal@gmail.com>
2022-12-29 20:22:20 +0100

    📦 NEW: importamos la funcion getPokemonOptions

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,12 @@
 </template>
 
 <script>
+
 import PokemonPage from './pages/PokemonPage.vue'
 import PokemonPicture from './components/PokemonPicture.vue'
 import PokemonOptions from './components/PokemonOptions.vue'
-
+import getPokemonOptions from './helpers/getPokemonOptions'
+getPokemonOptions()
 export default {
   name: 'App',
   components: {
