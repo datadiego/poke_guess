cae70d02f3e21f800bf450e3be75f15e79be4ae6
Juan Diego <juandiegomariscal@gmail.com>
2022-12-29 22:42:13 +0100

    👌 IMPROVE: componentes movidos a PokemonPage

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,16 @@
 <template>
   <PokemonPage/>
-  <PokemonPicture :pokemonid="4" :showpokemon="true" />
-  <PokemonOptions/>
 </template>
 
 <script>
 
 import PokemonPage from './pages/PokemonPage.vue'
-import PokemonPicture from './components/PokemonPicture.vue'
-import PokemonOptions from './components/PokemonOptions.vue'
-import getPokemonOptions from './helpers/getPokemonOptions'
-getPokemonOptions()
+
 export default {
   name: 'App',
   components: {
     PokemonPage, 
-    PokemonPicture, 
-    PokemonOptions
+
   }
 }
 </script>
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,12 +1,19 @@
 <template>
 <h1>¿Cual es este pokemon?</h1>
-
-
+<PokemonPicture :pokemonid="4" :showpokemon="true" />
+<PokemonOptions/>
 </template>
 
 <script>
+import PokemonPicture from '@/components/PokemonPicture.vue'
+import PokemonOptions from '@/components/PokemonOptions.vue'
+import getPokemonOptions from '@/helpers/getPokemonOptions'
 export default {
+    components:{
+        PokemonPicture, 
+        PokemonOptions,
 
+    }
 }
 </script>
 
